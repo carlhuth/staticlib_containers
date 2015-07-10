6cb2c93fe46a2b5ccfc5dd853c76778da2fa65c3
alex <alex@staticlibs.net>
2015-07-10 23:37:22 +0300

    1.2.0 docs

--- docs/html/search/all_74.js
+++ docs/html/search/all_74.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['take',['take',['../classstaticlib_1_1containers_1_1blocking__queue.html#a5e558aae7732e05e6a35d1e7d6e4581b',1,'staticlib::containers::blocking_queue']]]
+  ['take',['take',['../classstaticlib_1_1containers_1_1blocking__queue.html#a24a11714dbd9bfc4eaeec2c7c8628c4f',1,'staticlib::containers::blocking_queue']]]
 ];
---
+++ docs/html/search/all_75.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['unblock',['unblock',['../classstaticlib_1_1containers_1_1blocking__queue.html#a0553e01ce7d29d7113d6badfceeeb507',1,'staticlib::containers::blocking_queue']]]
+];
